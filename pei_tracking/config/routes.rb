8c4491a3d948269f86e275c5acac71ce6e5b8ed6
woarewe <rostislav.zhuravsky@gmail.com>
2017-10-17 01:01:49 +0300

    routes added

--- pei_tracking/config/routes.rb
+++ pei_tracking/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  resources :users, only: [:show] do
+    resources :capitals, :categories, only: [:create, :destroy, :new, :edit]
+  end
   get 'home/index'
 
   root 'home#index'
---
+++ pei_tracking/test/controllers/capitals_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CapitalsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
